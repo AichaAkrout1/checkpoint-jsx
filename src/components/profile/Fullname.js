3074fa547c6c0e9861cc7aae3f69138ab54a4162
AichaAkrout1 <aichadhouha@gmail.com>
2023-04-05 17:51:16 +0200

    first commit

---
+++ src/components/profile/Fullname.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fullname() {
+  return (
+    <div className='Name'>
+        <h2>AKROUT AICHA</h2>
+    </div>
+  )
+}
+
+export default Fullname
---
+++ src/components/profile/Profilephoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Profilephoto() {
+  return (
+    <div>
+        <img src="https://media.licdn.com/dms/image/D4E03AQGQ50oNQIrg7w/profile-displayphoto-shrink_400_400/0/1680276975900?e=1686182400&v=beta&t=9jbe2D225wgELxvooBViTeQYXSs2hHuIlDrqt_7keDg" alt=""></img>
+        
+    </div>
+  )
+}
+
+export default Profilephoto
